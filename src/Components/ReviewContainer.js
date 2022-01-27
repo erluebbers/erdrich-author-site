cc5e632bcb3ee7abebf7f392292a8e6c30b72ebe
Erik Luebbers <erluebbers@gmail.com>
2022-01-27 12:09:29 -0800

    update db structure and review components

--- src/Components/ReviewContainer.js
+++ src/Components/ReviewContainer.js
@@ -1,12 +1,14 @@
 import '../Components.css';
 import React from "react";
 import ReviewForm from "./ReviewForm"
+import ReviewList from "./ReviewList"
 
 function ReviewContainer() {
 
   return (
     <div>
       <ReviewForm />
+      <ReviewList />
     </div>
   );
 
